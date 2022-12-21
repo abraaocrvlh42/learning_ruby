d1a19c4b75ac9772786db318be23abf65cbcb2fe
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-12-21 14:35:32 -0300

    exercises

---
+++ exercises/basic_math_operations.rb
@@ -0,0 +1,12 @@
+# Basic math operations in ruby
+
+puts "-----------------------"
+puts "|   Math Operations   |"
+puts "-----------------------"
+
+print 'Enter first number: '
+num1 = gets.chomp.to_i
+
+print 'Enter second number: '
+num2 = gets.chomp.to_i
+
