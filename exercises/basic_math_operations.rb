dc0761bbbcd5eefe3f195e1af5a3dcb48998dcbb
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-12-22 07:56:38 -0300

    exercises

--- exercises/basic_math_operations.rb
+++ exercises/basic_math_operations.rb
@@ -5,8 +5,16 @@ puts "|   Math Operations   |"
 puts "-----------------------"
 
 print 'Enter first number: '
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 
 print 'Enter second number: '
-num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
 
+addition       = num1 + num2
+subtraction    = num1 - num2
+multiplication = num1 * num2 
+division       = num1 / num2
+potentiation   = num1 ** num2
+mod            = num1 % num2
+
+puts "Results of operations are:\n Addition: #{addition}\n Subtraction: #{subtraction}\n Multiplication: #{multiplication}\n Division: #{division}\n Pontentiation: #{potentiation}\n Module: #{mod}"
