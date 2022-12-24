266fe4f984a0e1f7cae859471780d0252a1e00fe
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-12-23 21:47:36 -0300

    basic language concepts

---
+++ ruby_concepts/string.rb
@@ -0,0 +1,8 @@
+# Strings are strings of characters, which we can create by delimiting those characters with single or double quotes, like
+# for example "blue" or 'blue', being able to use singles or doubles inside the other like "the sky is 'blue'" or 'the sky is "blue"'
+
+# example:
+
+name = 'Abraão Carvalho'
+
+puts name
