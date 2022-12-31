353c3d86a56f412e561fc139862cad0475b28f57
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-12-31 20:50:39 -0300

    basic language concepts

---
+++ ruby_concepts/while.rb
@@ -0,0 +1,15 @@
+# Ruby program to illustrate 'while' loop
+ 
+# variable x
+x = 4
+ 
+# using while loop
+# here conditional is x i.e. 4
+while x >= 1
+ 
+# statements to be executed
+  puts "GeeksforGeeks"
+  x = x - 1
+   
+# while loop ends here
+end
