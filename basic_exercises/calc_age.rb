599ee6f7ef202715794b50f033b215ba00bd7b9f
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-06 11:03:22 -0300

    Age finder

--- basic_exercises/calc_age.rb
+++ basic_exercises/calc_age.rb
@@ -3,9 +3,10 @@ result = ' '
 loop do
   puts "result"
 
+  puts "Select the desired option."
   puts "1) Find out age "
   puts "0) Exit"
-  print 'Exiting...'
+  print 'Option: '
 
   option = gets.chomp.to_i
 
@@ -18,6 +19,13 @@ loop do
 
     current_age = current_year - year_of_birth
 
-    puts ""
+    puts "Your current age is: #{current_age} years old !"
+
+  elsif
+    option == 0
+    break
+  else
+    result = "Invalid option. Enter 1 to find the age and 0 to exit."
   end
+  system "clear"
 end
