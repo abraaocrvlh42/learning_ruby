d277064970a8c385e7e24ba3034c4acf103ebb1c
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-06 10:46:01 -0300

    Age finder

--- basic_exercises/calc_age.rb
+++ basic_exercises/calc_age.rb
@@ -4,4 +4,20 @@ loop do
   puts "result"
 
   puts "1) Find out age "
+  puts "0) Exit"
+  print 'Exiting...'
+
+  option = gets.chomp.to_i
+
+  if option == 1
+    print 'Enter your year of birth: '
+    year_of_birth = gets.chomp.to_i
+
+    print 'Enter the current year: '
+    current_year = gets.chomp.to_i
+
+    current_age = current_year - year_of_birth
+
+    puts ""
+  end
 end
