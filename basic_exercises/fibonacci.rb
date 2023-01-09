9b4b4827bb1e4c4b3c9a9440a49efe1eee013162
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-08 22:46:01 -0300

    fibonacci

--- basic_exercises/fibonacci.rb
+++ basic_exercises/fibonacci.rb
@@ -1,15 +1,15 @@
 # Fibonacci in Ruby
 
 def fib(n)
-	if (n<=2)
+	if (n <= 2)
 		return 1
 	else
-		return (fib(n-1)+fib(n-2))
+		return (fib(n-1) + fib (n-2))
 	end
 end
 
 puts "Enter the number of terms:-"
-n=gets.chomp.to_i
+n = gets.chomp.to_i
 
 puts "The first #{n} terms of fibonnaci series are:-"
 for c in 1..n
