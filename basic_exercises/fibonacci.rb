55519ddf219b6066c5bb066f544f28830df231e5
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-08 22:40:54 -0300

    fibonacci

---
+++ basic_exercises/fibonacci.rb
@@ -0,0 +1,18 @@
+=begin 
+Ruby program to print Fibonacci series with recursion
+
+def fib(n)
+	if (n<=2)
+		return 1
+	else
+		return (fib(n-1)+fib(n-2))
+	end
+end
+
+puts "Enter the number of terms:-"
+n=gets.chomp.to_i
+
+puts "The first #{n} terms of fibonnaci series are:-"
+for c in 1..n
+	puts fib(c)
+end
