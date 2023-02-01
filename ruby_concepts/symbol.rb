5133c6c9e398f69974c851bd66921eb7ca88debe
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-01 18:34:07 -0300

    ruby exercises

--- ruby_concepts/symbol.rb
+++ ruby_concepts/symbol.rb
@@ -1,10 +1,10 @@
 # Converting Between Strings & Symbols
-#You can convert a symbol into a string when necessary.
-#Why would you want to do that?
-#Because symbols only have a subset of the methods that a String has.
-#And symbols can’t be changed, so if you want to work with the individual characters of the symbol then you want a string.
-#You can do this using the to_s method.
-#For example, when using method_missing you get the name of the missing method as a symbol. You may want to check if this method name matches a certain pattern (like ending in ?).
+# ou can convert a symbol into a string when necessary.
+# Why would you want to do that?
+# Because symbols only have a subset of the methods that a String has.
+# And symbols can’t be changed, so if you want to work with the individual characters of the symbol then you want a string.
+# You can do this using the to_s method.
+# For example, when using method_missing you get the name of the missing method as a symbol. You may want to check if this method name matches a certain pattern (like ending in ?).
 
 #Example:
 
