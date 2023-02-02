558a2b679439ab706a63bed0f4f5e9007d45d35e
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-01 21:39:09 -0300

    advanced exercises

--- advanced_exercises/schedule.rb
+++ advanced_exercises/schedule.rb
@@ -3,19 +3,22 @@
 # We can list all contacts or list just one.
 
 @schecule = [
-  {name: 'Abraão', phone_number: '943138424'}
-  {name: 'Jorge', phone_number: '987623510'},
+  {name: 'Dame', phone_number: '943138424'},
+  {name: 'Tomy', phone_number: '987623510'},
   {name: 'Juca', phone_number: '964821005'}
 ]
 
 # Method to see the complete list of contacts saved in the phonebook
 def all_contacts
-
+  @schecule.each do |contacts|
+    puts "| #{contacts[:name]} | #{contacts[:phone_number]} |"
+    puts "--------------------- "
+  end
 end
 
 # Method for adding new contacts
 def add_contacts
-
+  
 end
 
 # Method to view a specific contact
@@ -36,9 +39,14 @@ end
 loop do
   puts "1 - Contacts\n2 - Add Contacts\n3 - View Contacts\n4 - Edit Contatcs\n5 - Delet Contacs\n0 - Exit"
 
+  print 'Option: '
+
   options = gets.chomp.to_i
 
   case
+  when options == 0
+    puts "Exiting..."
+    break
   when options == 1
     all_contacts
   when options == 2
@@ -49,10 +57,7 @@ loop do
     edit_contacts
   when options == 5
     delete_contacts
-  elsif
-    options == 0
-    break
   else
-    'Invalid option.'
+    puts "Invalid option."
   end
 end
