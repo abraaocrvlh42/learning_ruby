bd6385aeeda5670cac1d43977fb32cc319eca3d1
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-06 19:00:19 -0300

    OOP in Ruby

--- OOP/class.rb
+++ OOP/class.rb
@@ -1,4 +1,18 @@
 class Student
   @name
   @age
+
+  def change_name(name)
+    @name = name
+  end
+
+  def show_name
+    @name
+  end
 end
+
+stud = Student.new
+
+stud.name('Abraão')
+
+puts stud.show_name
