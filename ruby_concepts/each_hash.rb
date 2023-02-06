b09b5024b8018a22c89cfb271eba6a9697f49713
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-06 17:18:46 -0300

    ruby exercises

--- ruby_concepts/each_hash.rb
+++ ruby_concepts/each_hash.rb
@@ -1,1 +1,7 @@
-#
+# Mapping hashs with each()
+
+myTeams = {:baskeball => 'Cleveland Cavaliers', :americam_football => 'New England Patriots', :baseball => 'New York Yankees', :soccer => 'Vasco da Gama'}
+
+myTeams.each do | key, value |
+  puts "#{key} #{value}"
+end
