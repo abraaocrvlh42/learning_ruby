6511ca8dc65456fba3326dd513fb15cbf1caac4e
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-19 10:53:49 -0300

    Polymorphism in Ruby

--- OOP/poly.rb
+++ OOP/poly.rb
@@ -9,7 +9,13 @@ class Keyboard < Instrument
 end
 
 class Pencil < Instrument
-  def Writing
+  def writing
     puts "Writing in pencil"
+  end
+end
+
+class Pen < Instrument
+  def writing
+    puts "Writing with pen"
   end
 end
