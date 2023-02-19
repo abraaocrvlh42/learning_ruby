5941dc4df76ba6e591014500134bdb629981bf1b
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-02-19 12:26:53 -0300

    advanced exercises

---
+++ advanced_exercises/athlete.rb
@@ -0,0 +1,22 @@
+class Atlhet
+  def power
+    'Very strong'
+  end
+
+  def speed
+    'Very speed'
+  end
+end
+
+class Basketball < Atlhet
+  def basketball_player
+  
+  end
+end
+
+class Soccer < Atlhet
+  def soccer_player
+  
+  end
+end
+
