ff521cb5991aa5288d6df221b0273419fa7b17c6
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-05-08 16:59:38 -0300

    advanced concepts

--- advanced_concepts/procs.rb
+++ advanced_concepts/procs.rb
@@ -1,5 +1,5 @@
 # There is no dedicated Lambda class. A lambda is just a special Proc object. If you take a look at the instance methods from Proc, you will notice there is a lambda? method.
-#A proc behaves differently than a lambda, specially when it comes to arguments:
+# A proc behaves differently than a lambda, specially when it comes to arguments:
 t = Proc.new { |x,y| puts "I don't care about arguments!" }
 t.call
 # "I don't care about arguments!"
