72413433b20b71c4381845951df5e85c42e6faee
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-07-24 22:01:25 -0300

    Examples for my articles

--- examples_for_articles/lambdas.rb
+++ examples_for_articles/lambdas.rb
@@ -1,11 +1,17 @@
-# Using the lambda keyword
-# Creating a lambda that multiplies two numbers
-multiply_lambda = lambda { |x, y| x * y }
+# # Using the lambda keyword
+# # Creating a lambda that multiplies two numbers
+# multiply_lambda = lambda { |x, y| x * y }
 
-# Calling the lambda
-result = multiply_lambda.call(4, 6)
-puts result # Output: 24
+# # Calling the lambda
+# result = multiply_lambda.call(4, 6)
+# puts result # Output: 24
 
 # Another Examples:
 
 # Using literal syntax
+# Creating a lambda that checks if a number is even
+even_lambda = -> (num) { num.even? }
+
+# Calling the lambda
+puts even_lambda.call(10) # Output: true
+puts even_lambda.call(7)  # Output: false
