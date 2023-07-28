7f69fc982b17fc47d4ada86a75e5fef12342c882
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-07-28 09:43:07 -0300

    Examples for my articles

--- examples_for_articles/lambdas.rb
+++ examples_for_articles/lambdas.rb
@@ -1,10 +1,10 @@
-# # Using the lambda keyword
-# # Creating a lambda that multiplies two numbers
-# multiply_lambda = lambda { |x, y| x * y }
+# Using the lambda keyword
+# Creating a lambda that multiplies two numbers
+multiply_lambda = lambda { |x, y| x * y }
 
-# # Calling the lambda
-# result = multiply_lambda.call(4, 6)
-# puts result # Output: 24
+# Calling the lambda
+result = multiply_lambda.call(4, 6)
+puts result # Output: 24
 
 # Another Examples:
 
